66a125f8321f8a525e541e4d042725fe9739dbcf
Rafal Kusz <rafal.kusz.96@gmail.com>
2021-07-08 18:22:17 +0200

    add new branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Gałąź junior_feature_v1!!!");
         System.out.println("Gałąź master!!!");
         System.out.println("Gałąź junior_feature_v2!!!");
+        System.out.println("Gałąź new_branch!!!");
 
     }
 
